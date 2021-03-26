3bfe4a4aecedd3d029b14eab4cac111afd800973
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-03-26 09:24:12 +0100

    fix bugs

--- src/pages/CtfIntro/index.jsx
+++ src/pages/CtfIntro/index.jsx
@@ -4,7 +4,7 @@ import styles from "./ctfIntro.module.css";
 
 const info = [
   "Det er jammen mye rart man kan skule i en hvit bakgrunn!",
-  "Send flaggene dine til konkurranser[at]abakus.no",
+  "Send flaggene dine til konkurranse[at]abakus.no",
   "https://s.ntnu.no/flag",
   "Visste du at det bare er 0.1% sjanse for at denne teksten vises?",
   "Visste du at et av verdens mest usikre passord ble funnet hele 2485216 ganger i lekkede passordlister?",
