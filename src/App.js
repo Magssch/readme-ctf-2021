01df975905ed37a68015319a3a4ccc0ea204b7e5
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2021-04-09 08:51:33 +0200

    Fix new landing page text

--- src/App.js
+++ src/App.js
@@ -20,20 +20,28 @@ function App() {
                 margin: "auto",
               }}
             >
+              <br />
               Velkommen til readmes store CTF-skattejakt! Her må du bruke dine
               CTF-kunnskaper for å løse diverse oppgaver. Dersom du kommer deg
               til et flagg så må du notere deg ned dette. Flaggene er på
               formatet <b>readmeCTF{"{XXXXXXXXXXXXXXX}"}</b>, og målet med
-              konkurransen er å samle flest av disse flaggene. Send inn flaggene
-              du finner på epost til konkurranse[alfakrøll]abakus.no med emnet
-              "Flagg fra CTF" og legg med ditt navn. De(n) som samler flest
-              flagg kan vinne gavekort på Foodora!
+              konkurransen er å samle flest av disse flaggene.
+              <br />
+              <h2>
+                Konkurransen er nå over, og vi gratulerer så mye til vinnerne av
+                Foodora-gavekort! Takk til alle som sendte inn flaggene de fant!
+              </h2>
             </div>
             <header className="start">
+              <br />
               <img src={logo} className="App-logo" alt="logo" />
               <br />
-              <br />
-              <Link to={"/ctf-intro"}>Start her</Link>
+              <h2>Ikke prøvd CTF-en enda?</h2>
+              <h1>
+                <Link to={"/ctf-intro"} className={"link"}>
+                  Prøv den her
+                </Link>
+              </h1>
             </header>
           </div>
         </Route>
